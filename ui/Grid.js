11fc01fbfa660c2877320f4deef96779a52ad485
Oliver Buchtala <oliver.buchtala@gmail.com>
2016-04-21 09:06:52 +0200

    Improvements in Grid component.

--- ui/Grid.js
+++ ui/Grid.js
@@ -16,7 +16,14 @@ Grid.Prototype = function() {
     if (this.props.mobile) {
       el.addClass('sm-mobile');
     }
-    el.append(this.props.children);
+    for (var i = 0; i < this.props.children.length; i++) {
+      var rowEl = this.props.children[i];
+      if (rowEl.ComponentClass !== Grid.Row) {
+        throw new Error('Expecting Grid.Row elements');
+      }
+      rowEl.props.columns = this.props.columns;
+      el.append(rowEl);
+    }
     return el;
   };
 };
@@ -34,7 +41,19 @@ Row.Prototype = function() {
 
   this.render = function($$) {
     var el = $$('div').addClass('se-row');
-    el.append(this.props.children);
+    for (var i = 0; i < this.props.children.length; i++) {
+      var cellEl = this.props.children[i];
+      if (cellEl.ComponentClass !== Grid.Cell) {
+        throw new Error('Expecting Grid.Cell elements');
+      }
+      // manual layout: $$(Cell, { columns: 1-12 })
+      // auto: using Grid configuration (3 columns)
+      //       $$(Grid, { columns: [2, 2, 8]})
+      if (!cellEl.props.columns && this.props.columns) {
+        cellEl.props.columns = this.props.columns[i] || 1;
+      }
+      el.append(cellEl);
+    }
     return el;
   };
 };
@@ -52,7 +71,13 @@ Cell.Prototype = function() {
 
   this.render = function($$) {
     var el = $$('div').addClass('se-cell');
-    el.addClass('sm-column-'+this.props.columns);
+    // The naming 'sm-column' is counter intuitive
+    // as this value describes a width
+    // maybe better sm-width?
+    // FIXME: The internal layout implementation needs to be known to the user.
+    // This should be abstracted by using a props 'colspan'
+    // which then is turned into an sm-column value
+    el.addClass('sm-columns-'+this.props.columns);
     el.append(this.props.children);
     return el;
   };
